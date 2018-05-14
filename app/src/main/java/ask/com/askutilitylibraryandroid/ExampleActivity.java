9aca07d6a6905dec1d124f361086bf74a328426c
flatmind <askpi92@gmail.com>
2018-05-14 17:08:21 +0530

    added dialog click listeners

--- app/src/main/java/ask/com/askutilitylibraryandroid/ExampleActivity.java
+++ app/src/main/java/ask/com/askutilitylibraryandroid/ExampleActivity.java
@@ -1,47 +1,57 @@
 package ask.com.askutilitylibraryandroid;
 
-import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
-
-import ask.com.asklibrary.DocumentPicker;
-import ask.com.asklibrary.FileUtils;
-import ask.com.asklibrary.PermissionCheck;
 
 public class ExampleActivity extends AppCompatActivity {
 
     private static final int STORAGE_PERMISSIONS_REQUEST = 901;
     private static final int DOCUMENT_PICK_REQUEST = 101;
-    private DocumentPicker documentPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.pick);
-        documentPicker = new DocumentPicker(this);
 
         /**
-         * Permission checker for single or multiple
+         * check single or multiple permissisions
          * */
-        PermissionCheck permissionCheck = new PermissionCheck(this);
-        permissionCheck.askPermissionIfMissing(STORAGE_PERMISSIONS_REQUEST, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_CONTACTS, Manifest.permission.ACCESS_FINE_LOCATION);
+       /* PermissionCheck permissionCheck = new PermissionCheck(this);
+        permissionCheck.askPermissionIfMissing(STORAGE_PERMISSIONS_REQUEST,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_CONTACTS, Manifest.permission.ACCESS_FINE_LOCATION);*/
 
+        /**
+         * Alert Dialog Click Listeners
+         * */
+      /*  AlertDialogUtils alertDialogUtils = new AlertDialogUtils(this);
+        alertDialogUtils.confirmationAlertDialog("Confirm", "Are u sure??");
+        alertDialogUtils.setDialogInteractionListener(new AlertDialogUtils.OnDialogInteraction() {
+            @Override
+            public void onOkPressed() {
+                Toast.makeText(ExampleActivity.this, "Ok presses", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelPressed() {
+
+            }
+        });*/
+
+        /**
+         * Document Picker Example
+         * */
+        /*documentPicker = new DocumentPicker(this);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /**
-                 * Document picker from file explorer for local as well as Google Drive
-                 * */
                 documentPicker.startPicker(DOCUMENT_PICK_REQUEST);
             }
-        });
+        });*/
     }
 
     @Override
@@ -63,12 +73,11 @@ public class ExampleActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK && requestCode == DOCUMENT_PICK_REQUEST) {
             try {
-                String filePath = documentPicker.filePath(data);
-                FileUtils.openDoc(ExampleActivity.this, filePath);
+//                String filePath = documentPicker.filePath(data);
+//                FileUtils.openDoc(ExampleActivity.this, filePath);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
-
 }
--- asklibrary/src/main/java/ask/com/asklibrary/AlertDialogUtils.java
+++ asklibrary/src/main/java/ask/com/asklibrary/AlertDialogUtils.java
@@ -4,13 +4,32 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 
 /**
  * Created by flatmind on 4/5/18.
  */
 
 public class AlertDialogUtils {
-    public static void SimpleAlertDialog(Context context, String title, String message) {
+
+    OnDialogInteraction dialogInteraction;
+    Context context;
+
+    public AlertDialogUtils(Context context) {
+        this.context = context;
+    }
+
+    public interface OnDialogInteraction {
+        void onOkPressed();
+
+        void onCancelPressed();
+    }
+
+    public void setDialogInteractionListener(OnDialogInteraction listener) {
+        this.dialogInteraction = listener;
+    }
+
+    public void simpleAlertDialog(String title, String message) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.AskDefaultDialogStyle);
         builder.setTitle(title);
         builder.setMessage(message);
@@ -23,7 +42,33 @@ public class AlertDialogUtils {
         builder.show();
     }
 
-    public static void SimpleAlertDialogFinishActivity(final Context context, String title, String message) {
+    public void confirmationAlertDialog(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.AskDefaultDialogStyle);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                try {
+                    dialogInteraction.onOkPressed();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.d("exception", "onClick: Activity must Implement DialogInteraction Interface");
+                }
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                dialogInteraction.onCancelPressed();
+            }
+        });
+        builder.show();
+    }
+
+    public void simpleAlertDialogFinishActivity(String title, String message) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.AskDefaultDialogStyle);
         builder.setTitle(title);
         builder.setMessage(message);
@@ -38,7 +83,7 @@ public class AlertDialogUtils {
         builder.show();
     }
 
-    public static void SimpleAlertDialog(Context context, String title, String message, int style) {
+    public void simpleAlertDialog(String title, String message, int style) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context, style);
         builder.setTitle(title);
         builder.setMessage(message);
